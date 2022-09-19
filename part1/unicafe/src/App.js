3e4a80dee93bd09cb35755bf1eaad28138c13a93
Greyti <47736158+Greyti8888@users.noreply.github.com>
2022-09-19 16:05:52 +0300

    1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,20 +2,27 @@ import { useState } from 'react'
 
 const Header = ({ text }) => <h1>{text}</h1>
 const Button = ({ text, handleClick }) => <button onClick={handleClick(text)}>{text}</button>
-const StatisticLine = ({ text, value }) => <div>{text} {value}</div>
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 const Statistics = ({ data }) => {
   const { good, neutral, bad, all, avg, positive } = data
   if (all === 0) return <p>No feedback given</p>
   else return (
-    <div>
-      <StatisticLine text={'good'} value={good} />
-      <StatisticLine text={'neutral'} value={neutral} />
-      <StatisticLine text={'bad'} value={bad} />
-      <StatisticLine text={'all'} value={all} />
-      <StatisticLine text={'average'} value={avg} />
-      <StatisticLine text={'positive'} value={positive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text={'good'} value={good} />
+        <StatisticLine text={'neutral'} value={neutral} />
+        <StatisticLine text={'bad'} value={bad} />
+        <StatisticLine text={'all'} value={all} />
+        <StatisticLine text={'average'} value={avg} />
+        <StatisticLine text={'positive'} value={positive} />
+      </tbody>
+    </table>
   )
 
 }
