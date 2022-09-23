9db93d60faf61a5a443cbb863be3104e824a35f2
Greyti <47736158+Greyti8888@users.noreply.github.com>
2022-09-23 18:53:20 +0300

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,12 @@
 import { useState } from 'react'
 
-const Contacts = ({ persons }) => {
-  const output = persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)
+const Contacts = ({ persons, filter }) => {
+  const output = persons.reduce((valid, person) => {
+    const filterPassed = !filter || person.name.toLowerCase().includes(filter)
+    if (filterPassed) valid.push(<div key={person.name}>{person.name} {person.number}</div>)
+    return valid
+  }, [])
+
   return (
     <div>
       {output}
@@ -9,12 +14,22 @@ const Contacts = ({ persons }) => {
   )
 }
 
+const Filter = ({ filter, handleFilter }) => {
+  return (
+    <input value={filter} onChange={handleFilter} />
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('');
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -36,9 +51,16 @@ const App = () => {
     setNewNumber(e.target.value)
   }
 
+  const handleFilter = (e) => {
+    console.log(typeof e.target.value);
+    setFilter(e.target.value.toLowerCase())
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter filter={filter} handleFilter={handleFilter} />
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleName} />
@@ -51,7 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Contacts persons={persons} />
+      <Contacts persons={persons} filter={filter} />
     </div>
   )
 }
