76ce08657e33e006c79bed7ec80b4a061fd533ea
Greyti <47736158+Greyti8888@users.noreply.github.com>
2022-09-25 16:36:41 +0300

    3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,6 +36,11 @@ app.get('/api/persons/:id', (req, res) => {
   if (person) res.json(person)
   else res.status(404).end()
 })
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
 
 app.get('/info', (req, res) => {
   res.send(
