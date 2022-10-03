cb049e9b6a66cbe31c241a3822895c532c3549b7
Greyti <47736158+Greyti8888@users.noreply.github.com>
2022-10-03 17:27:06 +0300

    4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const { MONGODB_URI } = require('./utils/config');
+const { info, error } = require('./utils/logger');
+const { errorHandler, requestLogger, unknownEndpoint } = require('./utils/middleware')
+
+const blogsRouter = require('./controllers/blogs')
+
+info('connecting to MongoDB')
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    info('connected to MongoDB')
+  })
+  .catch((err) => {
+    error('error connecting to MongoDB:', err.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+module.exports = app
+
---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,24 @@
+const { info, error } = require('./logger')
+
+const requestLogger = (request, response, next) => {
+  info('Method:', request.method)
+  info('Path:  ', request.path)
+  info('Body:  ', request.body)
+  info('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (err, request, response, next) => {
+  error(err.message)
+  next(err)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
