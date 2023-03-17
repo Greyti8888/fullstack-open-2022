fa24530892d48c5f5618e2b9c36d27a36a8af15f
Greyti <47736158+Greyti8888@users.noreply.github.com>
2023-03-17 20:04:39 +0200

    4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   res.json(blogs)
 })
 
@@ -11,8 +12,14 @@ blogsRouter.post('/', async (req, res) => {
   if (!blog.title || !blog.url) {
     return res.status(400).json('Bad Request')
   }
+  const user = await User.findOne({})
+  blog.user = user.id
+
   const newBlog = new Blog(blog)
   const result = await newBlog.save()
+
+  await User.updateOne({ _id: user.id }, { blogs: [...user.blogs, result.id] })
+
   res.status(201).json(result)
 })
 
