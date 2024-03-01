ffed1aac846daa911d4649abe81c72847adac39f
Greyti <47736158+Greyti8888@users.noreply.github.com>
2024-03-01 19:27:56 +0200

    8.13

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,6 +1,24 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const Author = require('./models/author')
+const Book = require('./models/book')
+
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 let authors = [
   {
@@ -27,20 +45,6 @@ let authors = [
     id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e'
   }
 ]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
- */
 
 let books = [
   {
@@ -94,10 +98,6 @@ let books = [
   }
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
-
 const typeDefs = `
   type Query {
     bookCount: Int,
@@ -108,7 +108,7 @@ const typeDefs = `
 
   type Book {
     title: String!,
-    author: String!,
+    author: Author!,
     published: String!,
     id: ID!,
     genres: [String!]!
@@ -155,13 +155,25 @@ const resolvers = {
   },
 
   Mutation: {
-    addBook: (_root, args) => {
-      const book = { ...args, id: uuid() }
-      if (!authors.find((author) => author.name === args.author)) {
-        authors.push({ name: args.author, id: uuid() })
+    addBook: async (_root, args) => {
+      try {
+        let author = await Author.findOne({ name: args.author })
+        if (!author) {
+          author = new Author({ name: args.author })
+          await author.save()
+        }
+        const book = new Book({ ...args, author })
+        await book.save()
+        return book
+      } catch (error) {
+        throw new GraphQLError('Saving book failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: [...args],
+            error
+          }
+        })
       }
-      books.push(book)
-      return book
     },
     editAuthor: (_root, args) => {
       const author = authors.find((author) => author.name === args.name)
@@ -186,7 +198,7 @@ const server = new ApolloServer({
 })
 
 startStandaloneServer(server, {
-  listen: { port: 4000 }
+  listen: { port: PORT }
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
