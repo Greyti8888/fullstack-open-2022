728046173fa15c6e3ce654ba7d86b0a09062420b
Greyti <47736158+Greyti8888@users.noreply.github.com>
2024-04-20 00:10:22 +0300

    9.24

--- part9/patientorFrontend/src/components/PatientPage.tsx
+++ part9/patientorFrontend/src/components/PatientPage.tsx
@@ -3,13 +3,15 @@ import { useParams } from "react-router-dom";
 
 import { Male, Female, Transgender } from "@mui/icons-material";
 
-import { Patient } from "../types";
+import { Diagnosis, Patient } from "../types";
 
 import patientService from "../services/patients";
+import diagnosesService from "../services/diagnoses";
 
 const PatientPage = () => {
   const id = useParams().id;
   const [patient, setPatient] = useState<Patient | undefined>();
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[] | undefined>();
 
   useEffect(() => {
     const fetchPatient = async () => {
@@ -19,6 +21,15 @@ const PatientPage = () => {
 
     fetchPatient();
   }, [id]);
+
+  useEffect(() => {
+    const fetchDiagnoses = async () => {
+      const diagnoses = await diagnosesService.getAll();
+      setDiagnoses(diagnoses);
+    };
+
+    fetchDiagnoses();
+  });
 
   if (!patient) return null;
 
@@ -46,7 +57,20 @@ const PatientPage = () => {
           <div key={entry.id}>
             <div>{entry.date}</div>
             <div>{entry.description}</div>
-            <div>{entry.diagnosisCodes?.map((code) => `[${code}]`)}</div>
+            {entry.diagnosisCodes && (
+              <ul>
+                {entry.diagnosisCodes?.map((code) => {
+                  const diagnosis = diagnoses?.find(
+                    (diagnosis) => diagnosis.code === code,
+                  );
+                  return (
+                    <li>
+                      {diagnosis?.code} {diagnosis?.name}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
           </div>
         );
       })}
